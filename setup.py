1af74386e2986f0a27e0879657f376f86de61bb5
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-01-01 15:00:40 +0100

    First release of PyPI package

---
+++ setup.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+    name = 'osmapi',
+    packages = ['osmapi'],
+    version = '0.2.19',
+    description = 'Python wrapper for the OSM API',
+    author = u'Etienne Chové',
+    author_email = 'chove@crans.org',
+    maintainer = 'Stefan Oderbolz',
+    maintainer_email = 'odi@readmore.ch',
+    url = 'https://github.com/metaodi/osmapi',
+    download_url = 'https://github.com/metaodi/osmapi/archive/v0.2.19.zip',
+    keywords = ['openstreetmap', 'osm', 'api'],
+    license = 'GPL v3',
+    classifiers = [],
+)

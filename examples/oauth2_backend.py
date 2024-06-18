af7ea9e00976341c4a693c221c78e980179a90af
Mateusz Konieczny <matkoniecz@gmail.com>
2024-06-18 10:37:48 +0200

    keep validator happy
    no idea why it is supposed to be better

--- examples/oauth2_backend.py
+++ examples/oauth2_backend.py
@@ -19,9 +19,9 @@ load_dotenv(find_dotenv())
 client_id = os.getenv("OSM_OAUTH_CLIENT_ID")
 client_secret = os.getenv("OSM_OAUTH_CLIENT_SECRET")
 
-if client_id == None:
+if client_id is None:
     raise
-if client_secret == None:
+if client_secret is None:
     raise
 
 # special value for redirect_uri for non-web applications

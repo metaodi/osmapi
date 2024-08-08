6023f3540207db809d153f02e879577c9e043f57
Stefan Oderbolz <oderbolz@gmail.com>
2024-08-08 10:54:48 +0200

    Add example for log output

---
+++ examples/log_output.py
@@ -0,0 +1,24 @@
+# install cli-oauth2 for requests:  pip install cli-oauth2
+import osmapi
+import logging
+from pprint import pformat
+import urllib3
+
+log = logging.getLogger(__name__)
+
+loglevel = logging.DEBUG
+logging.basicConfig(
+    format="%(asctime)s %(name)s %(levelname)-8s %(message)s",
+    level=loglevel,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.captureWarnings(True)
+
+# shut up DEBUG messages of specific loggers
+logging.getLogger(osmapi.dom.__name__).setLevel(logging.INFO)
+logging.getLogger(urllib3.__name__).setLevel(logging.INFO)
+
+
+api = osmapi.OsmApi(api="https://api06.dev.openstreetmap.org")
+node1 = api.NodeGet("1111")
+log.debug(pformat(node1))

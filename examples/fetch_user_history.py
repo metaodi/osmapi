f0c224cc4c0f583dd4889e6ad0da89144a092b9c
Alexey Vazhnov <vazhnov@boot-keys.org>
2024-10-05 01:56:54 +0200

    Add `examples/fetch_user_history.py`

---
+++ examples/fetch_user_history.py
@@ -0,0 +1,91 @@
+"""
+Fetch user history of changes from OpenStreetMap.
+
+If there are more than 100 changes, `osmapi` will repeat request till all
+changes will be fetched.
+
+See
+https://wiki.openstreetmap.org/wiki/API_v0.6#Query:_GET_/api/0.6/changesets
+and
+https://wiki.openstreetmap.org/wiki/API_v0.6#Capabilities:_GET_/api/capabilities
+for more details.
+"""
+
+import datetime
+import argparse
+import logging
+import json
+import osmapi
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--username",
+        required=True,
+        help="OpenStreetMap username. Should be url-encoded if has special characters.",
+    )
+    parser.add_argument("--filename", help="JSON file to store.")
+    parser.add_argument(
+        "--api",
+        default="https://api.openstreetmap.org/api/0.6/",
+        help="Set OpenStreetMap API URL. Use https://master.apis.dev.openstreetmap.org/api/0.6/ for experiments.",
+    )
+    parser.add_argument(
+        "--start",
+        metavar="YYYY-MM-DD",
+        # Can't use `datetime.date` here because of error in `osmapi.UserHistory`:
+        # TypeError: can't compare datetime.datetime to datetime.date
+        type=datetime.datetime.fromisoformat,
+        help="History start date (by default, fetch everything)",
+    )
+    parser.add_argument(
+        "--limit",
+        default=0,
+        metavar="INT",
+        type=int,
+        help="0 is unlimited. The result will probably be more than limit",
+    )
+    parser.add_argument(
+        "--loglevel",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set logging level: DEBUG, INFO, WARNING, ERROR, CRITICAL",
+    )
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=getattr(logging, args.loglevel),
+        format="%(levelname)s:%(funcName)s:%(message)s",
+    )
+    logging.debug("All parsed arguments:")
+    for arg, value in sorted(vars(args).items()):
+        logging.debug("Argument %s: %s", arg, value)
+    return args
+
+
+def main():
+    config = parse_args()
+    logging.debug("Script started!")
+    api = osmapi.OsmApi()
+    logging.warning("Limits (capabilities): %s", api.Capabilities()["changesets"])
+    if config.start:
+        history = api.UserHistory(
+            UserId=config.username, limit=config.limit, TimeFrom=config.start
+        )
+    else:
+        history = api.UserHistory(UserId=config.username, limit=config.limit)
+    if config.filename:
+        with open(config.filename, encoding="utf-8", mode="w") as f:
+            # `default=str` — to avoid an error
+            # "Object of type datetime is not JSON serializable"
+            json.dump(history, f, indent=4, default=str)
+    else:
+        for k, v in history.items():
+            print(k, ":", v)
+    logging.debug("Script finished!")
+
+
+if __name__ == "__main__":
+    main()

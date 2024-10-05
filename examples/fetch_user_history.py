238fb46100109ee5c6e18a6efa8ba59b7740886f
Alexey Vazhnov <vazhnov@boot-keys.org>
2024-10-05 12:50:38 +0200

    Add support to save CSV files and to load Python "pickle" file, which is good for experiments

--- examples/fetch_user_history.py
+++ examples/fetch_user_history.py
@@ -2,7 +2,15 @@
 Fetch user history of changes from OpenStreetMap.
 
 If there are more than 100 changes, `osmapi` will repeat request till all
-changes will be fetched.
+changes are be fetched.
+
+Also script can store the history data into "pickle" file format, so it is
+possible to load it again without fetching OSM API.
+
+Example of storing the history data into different formats:
+python3 ./examples/fetch_user_history.py --username 'My%20user' --loglevel INFO --filename /tmp/MyHistory.pickle
+python3 ./examples/fetch_user_history.py --username 'My%20user' --loglevel INFO --load-pickle /tmp/MyHistory.pickle --filename /tmp/1.csv
+python3 ./examples/fetch_user_history.py --username 'My%20user' --loglevel INFO --load-pickle /tmp/MyHistory.pickle --filename /tmp/1.json
 
 See
 https://wiki.openstreetmap.org/wiki/API_v0.6#Query:_GET_/api/0.6/changesets
@@ -14,6 +22,7 @@ for more details.
 import datetime
 import argparse
 import logging
+import csv
 import json
 import pickle
 import osmapi
@@ -30,7 +39,13 @@ def parse_args():
     )
     parser.add_argument(
         "--filename",
-        help="File to store, supported formats: JSON and pickle (selected by extension).",
+        metavar="FILENAME",
+        help="File to store, supported formats: JSON, CSV or pickle (selected by extension).",
+    )
+    parser.add_argument(
+        "--load-pickle",
+        metavar="FILENAME",
+        help="Instead of fetching the history data from OSM API, use previously stored 'pickle' file.",
     )
     parser.add_argument(
         "--api",
@@ -69,6 +84,15 @@ def parse_args():
     return args
 
 
+def load_pickle(filename: str):
+    """
+    Load "pickle" file, stored beforehand by `save_file`.
+    To have possibility to play with data without stressing out OSM API server.
+    """
+    with open(filename, "rb") as f:
+        return pickle.load(f)
+
+
 def save_file(filename: str, history: dict[dict]):
     """
     Handle storing a file in different formats, depending on filename extension.
@@ -86,21 +110,40 @@ def save_file(filename: str, history: dict[dict]):
     elif filename.endswith(".pickle"):
         with open(filename, mode="wb") as f:
             pickle.dump(history, f)
+    elif filename.endswith(".csv"):
+        with open(filename, encoding="utf-8", mode="w", newline="") as f:
+            fieldnames = ["id", "created_at", "tag"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction="ignore")
+            writer.writeheader()
+            for i in history:
+                writer.writerow(history[i])
     else:
-        logging.error("Use known file extension to save the file.")
+        logging.error("Use known file extension to save the file: .json, .csv or .pickle")
+
+
+def convert_data(history):
+    # PUT YOUR CODE HERE IF NEEDED
+    pass
 
 
 def main():
     config = parse_args()
     logging.debug("Script started!")
-    api = osmapi.OsmApi()
-    logging.warning("Limits (capabilities): %s", api.Capabilities()["changesets"])
-    if config.start:
-        history = api.UserHistory(
-            UserId=config.username, limit=config.limit, TimeFrom=config.start
+    if config.load_pickle:
+        logging.info(
+            "Instead of using OSM API, loading 'pickle' file '%s'", config.load_pickle
         )
+        history = load_pickle(config.load_pickle)
     else:
-        history = api.UserHistory(UserId=config.username, limit=config.limit)
+        api = osmapi.OsmApi()
+        logging.info("Limits (capabilities): %s", api.Capabilities()["changesets"])
+        if config.start:
+            history = api.UserHistory(
+                UserId=config.username, limit=config.limit, TimeFrom=config.start
+            )
+        else:
+            history = api.UserHistory(UserId=config.username, limit=config.limit)
+    convert_data(history)
     save_file(config.filename, history)
     logging.debug("Script finished!")
 

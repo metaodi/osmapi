49be5e751401da2648728d7f645d73eac0172934
Stefan Oderbolz <oderbolz@gmail.com>
2026-02-18 10:34:30 +0100

    Update examples to use the new syntax

--- examples/changesets.py
+++ examples/changesets.py
@@ -1,14 +1,17 @@
 import osmapi
 from pprint import pprint
 
+
 api = osmapi.OsmApi(api="https://api06.dev.openstreetmap.org")
 
+
 try:
-    api.ChangesetGet(111111111111)
+    api.changeset_get(111111111111)
 except osmapi.ApiError as e:
     print(f"Error: {e}")
     if e.status == 404:
         print("Changeset not found")
 
+
 print("")
-pprint(api.ChangesetGet(12345))
+pprint(api.changeset_get(12345))
